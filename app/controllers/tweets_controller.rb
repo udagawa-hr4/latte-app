ff92d7f2edded460928b760853d2963b5a4994ea
udagawa-hr4 <hiro619881022@gmail.com>
2021-04-08 18:47:20 +0900

    投稿機能の作成、トップページに動画、画像を表示できるように修正

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,21 @@
 class TweetsController < ApplicationController
   def index
+    @tweets = Tweet.includes(:user).order("created_at DESC")
   end
   def new
     @tweet = Tweet.new
+   
+  end
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  private
+  def tweet_params
+    params.require(:tweet).permit( :title, :text, images:[]).merge(user_id: current_user.id)
   end
 end
