172722c91d303bdded57f5c251ec3354c56f3681
udagawa-hr4 <hiro619881022@gmail.com>
2021-04-14 10:59:17 +0900

    編集ページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,21 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @tweets = @user.tweets.order("tweets.created_at DESC").paginate(page: params[:page], per_page: 8)
   end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if user_signed_in? && @user.id == current_user.id
+      if @user.update(profile_params)
+      redirect_to user_path(@user.id)
+      else
+      redirect_to action: :show
+      end
+    else
+      redirect_to root_path
+    end 
+  end
   private
   def profile_params
     params.require(:user).permit(:introduction,:name)
